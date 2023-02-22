0d336ef782f8ac1887d1ce8a30a09ebc8ac9d5cd
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-02-22 18:22:48 -0300

    Test: testes funcionando perfeitamente

--- test-with-node/src/controllers/usersController.test.ts
+++ test-with-node/src/controllers/usersController.test.ts
@@ -12,5 +12,25 @@ describe('Users Controller', ()=> {
         usersControler.listarUsuario(mockRequest, mockResponse)
         expect(mockResponse.state.status).toBe(200)
         //expect(mockResponse.state.status).toHaveLength(4)
-    } )
+    })
+
+    it('Deve criar um novo usuário', () => {
+        mockRequest.body = {
+            name: 'Novo usuário'
+          }
+      
+          usersControler.criarUsuario(mockRequest, mockResponse)
+          expect(mockResponse.state.status).toBe(201)
+          expect(mockResponse.state.json).toMatchObject({'mensagem': `Usuário Novo usuário criado`})
+    })
+
+    it('Não deve criar um usuário com o nome em branco', () => {
+        mockRequest.body = {
+          name: ''
+        }
+    
+        usersControler.criarUsuario(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(403)
+        expect(mockResponse.state.json).toMatchObject({mensagem: 'Não é possível criar usuários sem um nome'})
+      })
 })
--- test-with-node/src/controllers/usersController.ts
+++ test-with-node/src/controllers/usersController.ts
@@ -2,19 +2,19 @@ import { Request, Response } from 'express'
 import { database } from "../database"
 
 export class UsersController {
-    criarUsuario (request: Request, response: Response): Response{
-        const { nome } = request.body
-
-        if (nome.length < 1) {
-            return response.status(403).json({ mensagem: `Não é possível criar usuários em branco` })
-        }
-
-        database.push(nome)
-        return response.status(201).json({ 'mensagem': `Usuário criado` })
+    criarUsuario (request: Request, response: Response): Response {
+      const { name } = request.body
+    
+      if(name.length < 1){
+        return response.status(403).json({mensagem: 'Não é possível criar usuários sem um nome'})
+      }
+    
+      database.push(name)
+      return response.status(201).json({'mensagem': `Usuário ${name} criado`})
     }
-
+  
     listarUsuario (request: Request, response: Response): Response {
-        return response.status(200).json(database)
-    } 
-}
+      return response.status(200).json(database)
+    }
+  }
 
