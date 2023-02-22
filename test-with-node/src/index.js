08853aea9006555bc15d0fde9aed6cd996fbca6e
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-02-22 08:41:51 -0300

    Feat: metodo get criado

--- test-with-node/src/index.js
+++ test-with-node/src/index.js
@@ -1,6 +1,8 @@
-import { Express } from "express";
+import express from "express";
+import { routes } from "./routes.js";
 
-const server = Express();
+const server = express();
+server.use(routes);
 
 server.listen(5000, () => {
     console.log('Servidor on na porta 5000')
--- test-with-node/src/routes.js
+++ test-with-node/src/routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 const routes = Router()
 
-const database = []
+const database = ['Thais']
 
 routes.get('/', (request, response) => {
     return response.status(200).json(database)
 })
+
+
 
 
 
